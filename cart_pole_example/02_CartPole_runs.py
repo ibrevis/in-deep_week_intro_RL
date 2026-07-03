b15f5746cfacb1873e977eec26eb2787e45208b1
Ignacio Brevis <88429528+ibrevis@users.noreply.github.com>
2026-07-03 15:34:44 +0100

    cartpole visualizations updated

--- cart_pole_example/02_CartPole_runs.py
+++ cart_pole_example/02_CartPole_runs.py
@@ -5,12 +5,14 @@ env = gym.make("CartPole-v1", render_mode="human")
 
 for episode in range(10):
     state, _ = env.reset()
+    episode_return = 0.
     for step in range(100):
-        print(f"Episode: {episode}, Step: {step}")
         action = env.action_space.sample()  # Take a random action
         state, reward, terminated, truncated, _ = env.step(action)
+        episode_return += reward
         time.sleep(0.05)
         if terminated or truncated:
             time.sleep(2)
             break
+    print(f"Episode: {episode}, Total Return: {episode_return}")
 env.close()
